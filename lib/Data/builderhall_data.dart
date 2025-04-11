27b5e89ee7aee05b77b1b3c37613ee6520cd0e94
saad ahmed <saadahmed0147@gmail.com>
2025-04-11 22:29:54 +0500

    update builderhall data

---
+++ lib/Data/builderhall_data.dart
@@ -0,0 +1,177 @@
+final List<Map<String, dynamic>> builderHalls = const [
+  {
+    'title': 'Best Builderhall BH10\nBases BH10',
+    'image': 'assets/builderhall/bh10.png',
+    'baseNumber': 'BH10',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-10-Base-with-Link-Anti-Air-Anti-2-stars-Layout-Clash-of-Clans-BH10-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH10%3ABB2%3AAAAAUAAAAAG_itTE8QGMQ3lCqoui-R9X',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-10-Base-with-Link-Anti-3-stars-Layout-Clash-of-Clans-BH10-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH10%3ABB2%3AAAAAUAAAAAG_i2LfwBEIfF35wLhb4pne',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-10-Base-with-Link-Anti-Air-Layout-Clash-of-Clans-BH10-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH10%3ABB2%3AAAAAUAAAAAG_ivFypTbnH09-K-Pb2Kba',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH9\nBases BH9',
+    'image': 'assets/builderhall/bh9.png',
+    'baseNumber': 'BH09',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-9-Base-with-Link-Anti-3-stars-Layout-Clash-of-Clans-BH9-18.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH9%3ABB2%3AAAAATwAAAAG_S4tj4rxuTu0u5DuPN1UC',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-9-Max-Levels-Base-with-Link-Anti-Air-Layout-Clash-of-Clans-BH9-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH9%3ABB2%3AAAAATwAAAAG_QwLHh1zL-QRb31mpg6W4',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-9-Max-Levels-Base-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-Layout-Clash-of-Clans-BH9-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH9%3ABB2%3AAAAATwAAAAG_TOuzCu1xoygHMWeTQlx4',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH8\nBases BH8',
+    'image': 'assets/builderhall/bh8.png',
+    'baseNumber': 'BH08',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-8-Base-with-Link-Anti-Air-Layout-Clash-of-Clans-BH8-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3ABB2%3AAAAABgAAAAKxaWXHuMR9-uPeFjbOUOiU',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-8-Base-with-Link-Anti-Everything-Layout-Clash-of-Clans-BH8-13.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3ABB2%3AAAAABgAAAAKd_tN8VYcvYlXoItdZ-kgf',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-8-Base-with-Link-Anti-2-stars-Layout-Clash-of-Clans-BH8-12.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3ABB2%3AAAAABgAAAAKxaeIxSWyrBlXR_X4Ss1UV',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH7\nBases BH7',
+    'image': 'assets/builderhall/bh7.png',
+    'baseNumber': 'BH07',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-7-Base-with-Link-Anti-Air-Anti-3-stars-Layout-Clash-of-Clans-BH7-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH7%3ABB2%3AAAAAWwAAAAGqJTpoXHDwzkzJf34zkGcw',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-7-Base-with-Link-Anti-Everything-Layout-Clash-of-Clans-BH7-14.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH7%3ABB2%3AAAAARAAAAAHq61m75pfccDTImms1s5Yi',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-7-Max-Levels-Base-with-Link-Anti-Air-Layout-Clash-of-Clans-BH7-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH7%3ABB2%3AAAAAWwAAAAGqKPcPGw9IqQketi_wXUIR',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH6\nBases BH6',
+    'image': 'assets/builderhall/bh6.png',
+    'baseNumber': 'BH06',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Top-Builder-Hall-Level-6-Base-with-Link-Anti-Air-Layout-Clash-of-Clans-BH6-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH6%3ABB2%3AAAAAIwAAAAJfkXmrq-JwkQzb88rc7qft',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Builder-Hall-Level-6-Base-with-Link-Anti-Air-Anti-3-stars-Layout-Clash-of-Clans-BH6-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH6%3ABB2%3AAAAAIwAAAAJL6OoQhUqne1b6jvVIQIrU',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-6-Base-with-Link-Anti-3-stars-Layout-Clash-of-Clans-BH6-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH6%3ABB2%3AAAAAIwAAAAJfkZYa53jt70VpB2y8VQtd',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH5\nBases BH5',
+    'image': 'assets/builderhall/bh5.png',
+    'baseNumber': 'BH05',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-5-Base-with-Link-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-BH5-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH5%3ABB2%3AAAAAFwAAAAJuQiBF4I0FJ0x3E4SHkoNC',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-5-Base-with-Link-Anti-Air-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-BH5-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH5%3ABB2%3AAAAAFwAAAAJuQiqPUOC6kUzbse-cmU9U',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/05/Best-Builder-Hall-Level-5-Base-with-Link-Anti-air-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-BH5-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH5%3ABB2%3AAAAAFwAAAAJuQjXL2NozoOYCfxtnl4t1',
+      },
+    ],
+  },
+  {
+    'title': 'Best Builderhall BH4\nBases BH4',
+    'image': 'assets/builderhall/bh4.png',
+    'baseNumber': 'BH04',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/10/Best-Builder-Hall-Level-4-Base-with-Link-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-BH4-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3ABB2%3AAAAAKQAAAAJmPPRt0F_0dEcjy_HsOIpk',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/10/Builder-Hall-Level-4-Base-with-Link-Anti-3-stars-Layout-Clash-of-Clans-BH4-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3ABB2%3AAAAAKQAAAAJmPQA9Mrj7bWmIYJxMBcqn',
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/10/Builder-Hall-Level-4-Base-with-Link-Anti-Air-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-BH4-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3ABB2%3AAAAAKQAAAAJmPQoXwu-vpgBT64BfirlE',
+      },
+    ],
+  },
+];
