07b41d35734e7290fa8dfbf5727043b8b73ba623
saad ahmed <saadahmed0147@gmail.com>
2025-04-11 22:30:11 +0500

    update towmhall data

--- lib/Data/townhall_data.dart
+++ lib/Data/townhall_data.dart
@@ -3,70 +3,392 @@ final List<Map<String, dynamic>> townHalls = const [
     'title': 'Best TH17 Bases Townhall 17',
     'image': 'assets/townhall/th17.png',
     'baseNumber': 'TH17',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/11/Top-Farming-Base-TH17-with-Link-Hybrid-Anti-Air-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH17%3AHV%3AAAAAUAAAAAHUoMh1qDHFf8Uk_RUy_MY9',
+        'categories': ['Anti Air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/11/Top-Farming-Base-TH17-with-Link-Anti-2-Stars-Anti-3-stars-Hybrid-Layout-Clash-of-Clans-2024-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH17%3AHV%3AAAAAUAAAAAHUoK5l96up4IvUCPRC3DIy',
+        'categories': ['Anti 2 star', 'Farming', 'Hybrid'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/11/Top-Anti-3-stars-Base-TH17-with-Link-Legend-League-Anti-Air-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH17%3AHV%3AAAAAUAAAAAHUoEEQp00DXo7wpRLq9WxE',
+        'categories': ['Anti 2 star', 'Anti Air', 'Anti trophy'],
+      },
+    ]
   },
   {
     'title': 'Best TH16 Bases Townhall 16',
     'image': 'assets/townhall/th16.png',
     'baseNumber': 'TH16',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Farming-Base-TH16-with-Link-Hybrid-Anti-Air-Anti-2-stars-Anti-3-stars-Layout-Clash-of-Clans-01-2.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH16%3AHV%3AAAAADAAAAAKOv7fsWSORpmUpMgmMV0Ko',
+        'categories': ['Anti 2 star', 'Anti air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/12/Top-Farming-Base-TH16-with-Link-Anti-2-Stars-Anti-3-stars-Hybrid-Layout-Clash-of-Clans-2024-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH16%3AHV%3AAAAADAAAAAKOv8AEpDGy0bh2KBLC0qcV',
+        'categories': ['Anti 2 star', 'Farming', 'Hybrid'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/12/Top-Anti-3-stars-Base-TH16-with-Link-Legend-League-Anti-Air-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH16%3AHV%3AAAAADAAAAAKaBDq3elA1dpTYuZ8Z2gGZ',
+        'categories': ['Anti 2 star', 'Anti air', 'Anti trophy'],
+      },
+    ]
   },
   {
     'title': 'Best TH15 Bases Townhall 15',
     'image': 'assets/townhall/th15.png',
     'baseNumber': 'TH15',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Base-TH15-with-Link-Anti-2-stars-Anti-3-stars-Legend-League-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH15%3AHV%3AAAAATwAAAAHV87EROWTTie5ta5YuXfcy',
+        'categories': ['Anti 2 star', 'Hybrid'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH15-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-Electro-Dragon-WarCWL-Layout-Clash-of-Clans-04.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH15%3AHV%3AAAAATwAAAAHV859EIbIGdcDJFzekJnu5',
+        'categories': ['Anti 2 star', 'Anti air', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Farming-Base-TH15-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-Electro-Dragon-Hybrid-Layout-Clash-of-Clans-05.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH15%3AHV%3AAAAATwAAAAHV85NqjO3Sj3BcecCYEOTa',
+        'categories': ['Anti 2 star', 'Anti air', 'Farming'],
+      },
+    ]
   },
   {
     'title': 'Best TH14 Bases Townhall 14',
     'image': 'assets/townhall/th14.png',
     'baseNumber': 'TH14',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Farming-Base-TH14-with-Link-Anti-Air-Hybrid-Legend-League-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH14%3AHV%3AAAAAGwAAAAJ2TpO1gXxh9IfgvnCgAPVD',
+        'categories': ['Anti air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH14-with-Link-Anti-Air-Electro-Dragon-Anti-2-stars-Anti-3-stars-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH14%3AHV%3AAAAAGwAAAAJ2TpomZ3XT9PyE8jNkJqsS',
+        'categories': ['Anti 2 star', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Defense-Base-TH14-with-Link-Anti-2-stars-Anti-3-stars-Anti-Trophy-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH14%3AHV%3AAAAAGwAAAAKB8OitxUEZ1T5ePBQj8xoS',
+        'categories': ['Anti air', 'Anti trophy'],
+      },
+    ]
   },
   {
     'title': 'Best TH13 Bases Townhall 13',
     'image': 'assets/townhall/th13.png',
     'baseNumber': 'TH13',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Hybrid-Base-TH13-with-Link-Anti-Air-Anti-3-stars-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH13%3AHV%3AAAAASgAAAAHn-Nun2SBZJ_vb0k3nGamu',
+        'categories': ['Anti 3 star', 'Anti air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Farming-Base-TH13-with-Link-Anti-Air-Electro-Dragon-Anti-3-stars-Hybrid-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH13%3AHV%3AAAAASgAAAAHn-N5h1AYZsvSoAsPo4BBI',
+        'categories': ['Anti 3 star', 'Anti trophy', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Defence-Base-TH13-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH13%3AHV%3AAAAASgAAAAHn-OTMwGWzezz3yUFukcCp',
+        'categories': ['Anti 2 star', 'Anti air', 'Hybrid'],
+      },
+    ]
   },
   {
     'title': 'Best TH12 Bases Townhall 12',
     'image': 'assets/townhall/th12.png',
     'baseNumber': 'TH12',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Hybrid-Base-TH12-with-Link-Anti-3-stars-Anti-Air-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH12%3AHV%3AAAAABgAAAAK8eAlK4oilkmI5GI5Aud4p',
+        'categories': ['Anti 3 star', 'Anti air', 'Hybrid'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Anti-Loot-Base-TH12-with-Link-Anti-Air-Anti-3-stars-Unbeatable-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH12%3AHV%3AAAAABgAAAAK8eA0AL16oMLsgVh7i07yA',
+        'categories': ['Anti 3 star', 'Anti air', 'Anti trophy'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH12-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-Electro-Dragon-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH12%3AHV%3AAAAABgAAAAK8eBCLJPqCcXF8Z7zauRNc',
+        'categories': ['Anti 2 star', 'Anti air', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH11 Bases Townhall 11',
     'image': 'assets/townhall/th11.png',
     'baseNumber': 'TH11',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Defense-Base-TH11-with-Link-Anti-3-stars-Anti-Air-Hybrid-Anti-Trophy-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH11%3AHV%3AAAAARAAAAAIFlVMLiJyE4zLHXXI0ecVI',
+        'categories': ['Anti air', 'Anti trophy', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH11-with-Link-Anti-Air-Anti-Everything-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH11%3AHV%3AAAAARAAAAAIFlVuFlbknNI3a3RXy6X9a',
+        'categories': ['Anti 2 star', 'Anti air', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-Farming-Base-TH11-with-Link-Anti-2-stars-Anti-Air-Hybrid-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH11%3AHV%3AAAAARAAAAAIFlWaIox-tP_NrZcmjngaz',
+        'categories': ['Anti air', 'Farming'],
+      },
+    ]
   },
   {
     'title': 'Best TH10 Bases Townhall 10',
     'image': 'assets/townhall/th10.png',
     'baseNumber': 'TH10',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH10-with-Link-Anti-Air-Dragon-WarCWL-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH10%3AHV%3AAAAAWwAAAAG0glOO-sgtOD03WtPIMAfU',
+        'categories': ['Anti air', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH10-with-Link-Anti-Air-Legend-League-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH10%3AHV%3AAAAAWwAAAAG0gmDxA8uQFFvp0nF-zvRO',
+        'categories': ['War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH10-with-Link-Hybrid-Anti-3-stars-Anti-Air-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH10%3AHV%3AAAAAWwAAAAG0goAcRG-SZDAi0CI6nnwb',
+        'categories': ['Anti 3 star', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH9 Bases Townhall 9',
     'image': 'assets/townhall/th9.png',
-    'baseNumber': 'TH09',
+    'baseNumber': 'TH9',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH9-with-Link-Anti-Air-WarCWL-layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH9%3AHV%3AAAAAFwAAAAJ6ll-pTgXyaEItnGsrvzZv',
+        'categories': ['War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH9-with-Link-Anti-Air-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH9%3AHV%3AAAAAFwAAAAJ6lmsnxh73P-LA9PkcqXSK',
+        'categories': ['Anti air', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH9-with-Link-Anti-Air-Hybrid-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH9%3AHV%3AAAAAFwAAAAJ6lnGQDHJ56KzrrqIaZGlF',
+        'categories': ['Anti air', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH8 Bases Townhall 8',
     'image': 'assets/townhall/th8.png',
-    'baseNumber': 'TH08',
+    'baseNumber': 'TH8',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH8-with-Link-Anti-2-Stars-Anti-3-Stars-Anti-Air-WarCWL-layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3AHV%3AAAAAXAAAAAGgUHxgGIaX3ex8mwkLFONj',
+        'categories': ['Anti 2 star', 'Anti air', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH8-with-Link-Anti-2-stars-Anti-3-stars-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3AHV%3AAAAAXAAAAAGgUI8rA6AZNyIGB5pTn-Fd',
+        'categories': ['Anti 2 star', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH8-with-Link-Anti-Air-Electro-Dragon-Anti-2-stars-Anti-3-stars-WarCWL-layout-Clash-of-Clans03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH8%3AHV%3AAAAAXAAAAAGgUJKknvgFlIGdwJCYIOAd',
+        'categories': ['Anti 2 star', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH7 Bases Townhall 7',
     'image': 'assets/townhall/th7.png',
-    'baseNumber': 'TH07',
+    'baseNumber': 'TH7',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Defense-Anti-Trophy-Base-TH7-with-Link-Anti-Air-WarCWL-layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH7%3AHV%3AAAAAKQAAAAJqq9FoBcBxmarJbsVzqiJA',
+        'categories': ['Anti air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Defense-Anti-Trophy-Base-TH7-with-Link-Anti-Air-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH7%3AHV%3AAAAAKQAAAAJqrAhHpZ4BFG5UhnkC5dh6',
+        'categories': ['Anti air', 'Farming'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH7-with-Link-Anti-Air-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH7%3AHV%3AAAAAKQAAAAJqrBFTEu_qHQOzo4qE0KXY',
+        'categories': ['Anti air', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH6 Bases Townhall 6',
     'image': 'assets/townhall/th6.png',
-    'baseNumber': 'TH06',
+    'baseNumber': 'TH6',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH6-with-Link-WarCWL-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH6%3AWB%3AAAAAMQAAAAI8Sy9KYIVJGlJTm1o4b2g9',
+        'categories': ['War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH6-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-WarCWL-layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH6%3AWB%3AAAAAMQAAAAI8SzYnfwM6MHfEqwkxVgr_',
+        'categories': ['Anti 2 star', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH6-with-Link-Hybrid-Anti-2-Stars-Anti-3-Stars-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en?action=OpenLayout&id=TH6%3AWB%3AAAAAMQAAAAI8Sz8q40aHiVr8Ce6Mx_lI',
+        'categories': ['Anti 2 star', 'Anti air', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH5 Bases Townhall 5',
     'image': 'assets/townhall/th5.png',
-    'baseNumber': 'TH05',
+    'baseNumber': 'TH5',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH5-with-Link-Anti-3-stars-Anti-air-WarCWL-layouts-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH5%3AHV%3AAAAAJAAAAAJhBdxJvWSjouuT54gnXq64',
+        'categories': ['TH05', 'Anti 3 star', 'Farming', 'Hybrid'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH5-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-WarCWL-Layouts-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH5%3AHV%3AAAAAJAAAAAJcf9JVXfo-F_7Po2uRGv6m',
+        'categories': ['Anti 2 star', 'War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH5-with-Link-Anti-2-stars-Anti-3-stars-Anti-Air-WarCWL-Layouts-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH5%3AWB%3AAAAAJAAAAAJhBreZIJO69m2EGs8baI3Q',
+        'categories': ['Anti 2 star', 'War'],
+      },
+    ]
   },
   {
     'title': 'Best TH4 Bases Townhall 4',
     'image': 'assets/townhall/th4.png',
-    'baseNumber': 'TH04',
+    'baseNumber': 'TH4',
+    'layouts': [
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH4-with-Link-Anti-Air-WarCWL-Layout-Clash-of-Clans-01.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3AWB%3AAAAAIgAAAAJhBJlWbYSobOlCmudeyeO2',
+        'categories': ['War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH4-with-Link-Anti-Air-WarCWL-Layout-Clash-of-Clans-02.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3AHV%3AAAAAIgAAAAJhBJ6f-v229BXRBYJ4omsF',
+        'categories': ['War'],
+      },
+      {
+        'image':
+            'https://bases-coc.com/wp-content/uploads/2024/04/Top-War-Base-TH4-with-Link-Hybrid-Anti-3-stars-WarCWL-Layout-Clash-of-Clans-03.webp',
+        'link':
+            'https://link.clashofclans.com/en/?action=OpenLayout&id=TH4%3AHV%3AAAAAIgAAAAJhBKjQdZDQIlTsdAIrfNUE',
+        'categories': ['Anti air', 'War'],
+      },
+    ]
   },
 ];
