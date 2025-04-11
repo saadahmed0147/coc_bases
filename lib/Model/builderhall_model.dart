d665816eae48d7e5284187a4e3d548b41a7fa000
saad ahmed <saadahmed0147@gmail.com>
2025-04-11 22:30:22 +0500

    created model

---
+++ lib/Model/builderhall_model.dart
@@ -0,0 +1,58 @@
+class Layout {
+  final String image;
+  final String link;
+
+  Layout({
+    required this.image,
+    required this.link,
+  });
+
+  factory Layout.fromJson(Map<String, dynamic> json) {
+    return Layout(
+      image: json['image'],
+      link: json['link'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'image': image,
+      'link': link,
+    };
+  }
+}
+
+class BuilderHall {
+  final String title;
+  final String image;
+  final String baseNumber;
+  final List<Layout> layouts;
+
+  BuilderHall({
+    required this.title,
+    required this.image,
+    required this.baseNumber,
+    required this.layouts,
+  });
+
+  factory BuilderHall.fromJson(Map<String, dynamic> json) {
+    var list = json['layouts'] as List;
+    List<Layout> layoutList = list.map((i) => Layout.fromJson(i)).toList();
+
+    return BuilderHall(
+      title: json['title'],
+      image: json['image'],
+      baseNumber: json['baseNumber'],
+      layouts: layoutList,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'image': image,
+      'baseNumber': baseNumber,
+      'layouts': layouts.map((e) => e.toJson()).toList(),
+    };
+  }
+}
